e74648c1b4c28400feaa4db038e4b36cc8e9ea2e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-06-16 10:22:04 -0600

    completed searching.py file

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,8 +8,14 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
-
-
+    middle_index = (len(arr) // 2) - 1
+    current = middle_index
+    while current > 0 and current < len(arr)-1:
+        if target == arr[current]:
+            return current
+        elif target < arr[current]:
+            current -= 1
+        elif target > arr[current]:
+            current += 1
     return -1  # not found
